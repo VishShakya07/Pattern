8b6a9c05a35c2abe33aca15f1448c2af5102b4c4
Vishal Shakya <vishal.2426mca1786@kiet.edu>
2026-01-20 12:05:02 +0530

    Add files via upload

---
+++ print_L_5_star.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    for(int i=0; i<4; i++){
+        printf("*");
+        printf("\n");
+    }
+    for (int i = 0; i < 5; i++) {
+        printf("* "); 
+    }
+    printf("\n"); 
+
+    return 0;
+}
